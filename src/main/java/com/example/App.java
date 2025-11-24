e876a922e00bbc2aff7af6c3d58ff818a232b2b7
EC2 Default User <ec2-user@ip-172-31-31-156.ap-south-1.compute.internal>
2025-11-24 11:41:31 +0000

    added sleep to run app for long time

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,13 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello!!!! Welcome to JAVA  World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!! Welcome to JAVA World!");
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
+
